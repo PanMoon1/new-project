441819a824e36fe272e83751876c5fd9267d0cb0
PanMoon1 <139225612+PanMoon1@users.noreply.github.com>
2023-08-22 13:19:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("Hello, ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
