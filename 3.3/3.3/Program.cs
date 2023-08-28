3323fd17e1c0e30624a47fdcd98ad1c77838e7f6
PanMoon1 <139225612+PanMoon1@users.noreply.github.com>
2023-08-28 12:21:14 +0300

    Add files via upload

---
+++ 3.3/3.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            int n;
+            Console.WriteLine("Введите число");
+            n = int.Parse(Console.ReadLine());
+            bool prost = false;
+            while (i < n)
+            {
+                if (n % i == 0) prost = true;
+                i++;
+            }
+            if (prost)
+                Console.WriteLine("Ваше число не простое");
+            else
+                Console.WriteLine("Ваше число простое");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3.5/3.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int k = -1;
+            string s;
+            Console.WriteLine("Введите максимальное число");
+            n = int.Parse(Console.ReadLine());
+            Random rand = new Random();
+            int number = rand.Next(0, n+1);
+            while (k != number)
+            {
+                s = Console.ReadLine();
+                if (s == "") break;
+                k = int.Parse(s);
+                if (k < number) Console.WriteLine("Ваше число меньше загаданного");
+                else if (k > number) Console.WriteLine("Ваше число больше загаданного");
+            }
+            Console.WriteLine("Загаданное число  " + number);
+            Console.ReadKey();
+        }
+    }
+}
