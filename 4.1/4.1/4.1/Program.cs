09d167fe9604c251ac45cdd93c68ebedb0787ba7
PanMoon1 <139225612+PanMoon1@users.noreply.github.com>
2023-12-10 15:15:07 +0300

    Add files via upload

---
+++ 4.1/4.1/4.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+            Random r = new Random();
+            Console.WriteLine("Введите количество строк");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов");
+            b = int.Parse(Console.ReadLine());
+            int[,] m = new int[a, b];
+            sum = 0;
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    m[i, j] = r.Next(0, 9);
+                    Console.Write("  " + m[i, j]);
+                    sum += m[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Сумма всех элементов   " + sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4.2/4.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Random r = new Random();
+            Console.WriteLine("Введите количество строк");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов");
+            b = int.Parse(Console.ReadLine());
+            int[,] m = new int[a, b];
+            int[,] n = new int[a, b];
+            int[,] l = new int[a, b];
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    m[i, j] = r.Next(0, 4);
+                    n[i, j] = r.Next(0, 4);
+                    l[i, j] = m[i, j] + n[i, j];
+                    Console.Write("  " + l[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
