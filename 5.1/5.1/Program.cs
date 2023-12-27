753f24828f7b03af1851c53e98a2fb09babde6a7
PanMoon1 <139225612+PanMoon1@users.noreply.github.com>
2023-12-27 16:01:37 +0300

    Add files via upload

---
+++ 5.1/5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            string[] words;
+            text = Console.ReadLine();
+            words = SplitText(text);
+            Show(words);
+            Console.ReadKey();
+
+        }
+
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+
+        static void Show(string[] words)
+        {
+            foreach(string s in words)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ 5.2/5.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            text = Console.ReadLine();
+            text = Reverse(text);
+            Console.Write(text);
+            Console.ReadKey();
+
+        }
+
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+
+        static string Reverse(string text)
+        {
+            string[] words;
+            string Rtext = "";
+            words = SplitText(text);
+            for(int i = 0; i < words.Length/2; i++)
+            {
+                string s;
+                s = words[i];
+                words[i] = words[words.Length - i - 1];
+                words[words.Length - i - 1] = s;
+            }
+            for (int i = 0; i < words.Length; i++)
+                Rtext += (words[i] + " ");
+            return Rtext;
+        }
+    }
+}
